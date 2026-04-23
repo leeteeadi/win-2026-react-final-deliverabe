cfd9cd37735569633910db589fb0ead10f16a6b1
Angelina Roundfield <aroundfi@umich.edu>
2026-04-22 22:13:48 -0400

    Fix base path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/win-2026-react-final-deliverable/',
+  base: '/win-2026-react-final-deliverabe/',
 })
